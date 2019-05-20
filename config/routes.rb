9d9f831f753f1b5f93b07cb5b916f5f519c6180b
joyceyskwon <joyceyskwon@gmail.com>
2019-05-20 15:27:53 -0400

    final before heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+  # Serve websocket cable requests in-process
+  mount ActionCable.server => '/cable'
+
   resources :users, only: [:index, :show, :create]
   resources :chatrooms, only: [:index, :show, :create, :destroy]
   resources :messages, only: [:index, :show, :create, :destroy]
